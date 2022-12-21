w7br/nextjs-dashboard

commit 70520d6098892fe0e4e31d76f29d3f20adb7ca66

Refactor claim-conditions.tsx (#745)
* Fix emotion's string not allowed as css prop error in UpdateNotice

* Move components from claim-conditions.tsx to separate files

* Fix imports of PriceInput and QuantityInputWithUnlimited components

program-ui/common/program-claim-conditions.tsx
@@ -20,10 +20,8 @@ import {
 import { NFTDropUpdateableConditionsInputSchema } from "@thirdweb-dev/sdk/solana";
 import { TransactionButton } from "components/buttons/TransactionButton";
 import { BasisPointsInput } from "components/inputs/BasisPointsInput";
-import {
-  PriceInput,
-  QuantityInputWithUnlimited,
-} from "contract-ui/tabs/claim-conditions/components/claim-conditions";
+import { PriceInput } from "contract-ui/tabs/claim-conditions/components/price-input";
+import { QuantityInputWithUnlimited } from "contract-ui/tabs/claim-conditions/components/quality-input-with-unlimited";
 import { useTrack } from "hooks/analytics/useTrack";
 import { useTxNotifications } from "hooks/useTxNotifications";
 import { useMemo, useState } from "react";

contract-ui/tabs/claim-conditions/components/price-input.tsx
@@ -1,0 +1,49 @@
+import { Input, InputGroup, InputProps } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+interface PriceInputProps
+  extends Omit<InputProps, "onChange" | "value" | "onBlur" | "max" | "min"> {
+  value: number;
+  onChange: (value: number) => void;
+}
+
+export const PriceInput: React.FC<PriceInputProps> = ({
+  value = 0,
+  onChange,
+  isDisabled,
+  ...restInputProps
+}) => {
+  const [stringValue, setStringValue] = useState<string>(
+    isNaN(value) ? "0" : value.toString(),
+  );
+
+  useEffect(() => {
+    if (value !== undefined) {
+      setStringValue(value.toString());
+    }
+  }, [value]);
+  useEffect(() => {
+    const parsed = parseFloat(stringValue);
+    if (!isNaN(parsed)) {
+      onChange(parsed);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stringValue]);
+
+  return (
+    <InputGroup {...restInputProps}>
+      <Input
+        isDisabled={isDisabled}
+        value={stringValue}
+        onChange={(e) => setStringValue(e.target.value)}
+        onBlur={() => {
+          if (!isNaN(value)) {
+            setStringValue(value.toString());
+          } else {
+            setStringValue("0");
+          }
+        }}
+      />
+    </InputGroup>
+  );
+};

contract-ui/tabs/claim-conditions/components/quality-input-with-unlimited.tsx
@@ -1,0 +1,85 @@
+import {
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Button } from "tw-components";
+
+interface QuantityInputWithUnlimitedProps
+  extends Omit<InputProps, "onChange" | "value" | "onBlur" | "max" | "min"> {
+  value: string;
+  onChange: (value: string) => void;
+  hideMaxButton?: true;
+  decimals?: number;
+}
+
+export const QuantityInputWithUnlimited: React.FC<
+  QuantityInputWithUnlimitedProps
+> = ({
+  value = "0",
+  onChange,
+  hideMaxButton,
+  isDisabled,
+  isRequired,
+  decimals,
+  ...restInputProps
+}) => {
+  const [stringValue, setStringValue] = useState<string>(
+    isNaN(Number(value)) ? "0" : value.toString(),
+  );
+
+  useEffect(() => {
+    if (value !== undefined) {
+      setStringValue(value.toString());
+    }
+  }, [value]);
+
+  const updateValue = (_value: string) => {
+    if (_value === "") {
+      onChange(_value);
+      setStringValue(_value);
+      return;
+    }
+
+    setStringValue(_value);
+    onChange(_value);
+  };
+
+  return (
+    <InputGroup {...restInputProps}>
+      <Input
+        isRequired={isRequired}
+        isDisabled={decimals === undefined || isDisabled}
+        value={stringValue === "unlimited" ? "Unlimited" : stringValue}
+        onChange={(e) => updateValue(e.currentTarget.value)}
+        onBlur={() => {
+          if (value === "unlimited") {
+            setStringValue("unlimited");
+          } else if (!isNaN(Number(value))) {
+            setStringValue(Number(Number(value).toFixed(decimals)).toString());
+          } else {
+            setStringValue("0");
+          }
+        }}
+      />
+      {hideMaxButton ? null : (
+        <InputRightElement w="auto">
+          <Button
+            isDisabled={decimals === undefined || isDisabled}
+            colorScheme="primary"
+            variant="ghost"
+            size="sm"
+            mr={1}
+            onClick={() => {
+              updateValue("unlimited");
+            }}
+          >
+            Unlimited
+          </Button>
+        </InputRightElement>
+      )}
+    </InputGroup>
+  );
+};

commit aa8b5025952142f441af7538cc5ab95606fcd91e

Fix file name of QuantityInputWithUnlimited component (#746)
Fix the wrong file name introduced in #745

contract-ui/tabs/claim-conditions/components/claim-conditions-form.tsx
@@ -1,6 +1,6 @@
 import { ClaimConditionsProps } from "./claim-conditions";
 import { PriceInput } from "./price-input";
-import { QuantityInputWithUnlimited } from "./quality-input-with-unlimited";
+import { QuantityInputWithUnlimited } from "./quantity-input-with-unlimited";
 import { AdminOnly } from "@3rdweb-sdk/react/components/roles/admin-only";
 import { useIsAdmin } from "@3rdweb-sdk/react/hooks/useContractRoles";
 import {

program-ui/common/program-claim-conditions.tsx
@@ -21,7 +21,7 @@ import { NFTDropUpdateableConditionsInputSchema } from "@thirdweb-dev/sdk/solana
 import { TransactionButton } from "components/buttons/TransactionButton";
 import { BasisPointsInput } from "components/inputs/BasisPointsInput";
 import { PriceInput } from "contract-ui/tabs/claim-conditions/components/price-input";
-import { QuantityInputWithUnlimited } from "contract-ui/tabs/claim-conditions/components/quality-input-with-unlimited";
+import { QuantityInputWithUnlimited } from "contract-ui/tabs/claim-conditions/components/quantity-input-with-unlimited";
 import { useTrack } from "hooks/analytics/useTrack";
 import { useTxNotifications } from "hooks/useTxNotifications";
 import { useMemo, useState } from "react";